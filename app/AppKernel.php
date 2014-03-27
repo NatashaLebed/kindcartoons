9b026156a399a60aa6aae78a1602491031d1b532
NatashaLebed <natasha.lebed@gmail.com>
2014-03-27 11:51:30 +0200

    FOSUserBundle | Login, Register

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Lebed\UserBundle\LebedUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Lebed/VideoBundle/Controller/DefaultController.php
+++ src/Lebed/VideoBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Lebed\VideoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -11,5 +12,32 @@ class DefaultController extends Controller
         $videos = $this->getDoctrine()->getRepository('LebedVideoBundle:Video')->findAll();
 
         return $this->render('LebedVideoBundle:Default:index.html.twig', array('videos' => $videos));
+    }
+
+    public function categoryTreeAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('LebedVideoBundle:Category');
+        $options = array(
+            'decorate' => true,
+            'nodeDecorator' => function($node) {
+                    return '<a href="/category/'.$node['id'].'">'.$node['title'].'</a>';
+                }
+        );
+        $htmlTree = $repo->childrenHierarchy(null, false,  $options);
+
+        return new Response($htmlTree);
+    }
+
+    public function videosOfCategoryAction($id)
+    {
+        $videos = $this->getDoctrine()->getRepository('LebedVideoBundle:Video')
+            ->findByCategory($id);
+
+        if (!$videos) {
+            throw $this->createNotFoundException('No posts found');
+        }
+
+        return $this->render('LebedVideoBundle:Default:index.html.twig', array('videos'=>$videos));
     }
 }
