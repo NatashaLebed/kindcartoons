ab8e4413f75bd2221497c20c9604e544696b1d68
NatashaLebed <natasha.lebed@gmail.com>
2014-04-16 12:05:32 +0300

    Childmode

--- src/Lebed/VideoBundle/Controller/DefaultController.php
+++ src/Lebed/VideoBundle/Controller/DefaultController.php
@@ -198,4 +198,75 @@ class DefaultController extends Controller
         }
         return new Response(json_encode($res));
     }
+
+    public function childAction(Request $request)
+    {
+        if ($this->getRequest()->isXmlHttpRequest())
+        {
+            $this->get('session')->set('child', true);
+            $this->get('session')->set('time', $this->getRequest()->get('time'));
+            $this->get('session')->set('time_setup', $this->getRequest()->get('time_setup'));
+            $res = array(
+                'status' => 'Success',
+                'content' => 'Детский режим включен'
+            );
+        }
+        else
+        {
+            $res = array(
+                'status' => 'Error',
+                'content' => "Not AJAX request"
+            );
+        }
+
+        return new Response(json_encode($res));
+    }
+
+    public function offChildAction(Request $request)
+    {
+        if ($this->getRequest()->isXmlHttpRequest())
+        {
+            $this->get('session')->remove('child');
+            $this->get('session')->remove('time');
+            $this->get('session')->remove('time_setup');
+            $res = array(
+                'status' => 'Success',
+                'content' => 'Детский режим выключен'
+            );
+        }
+        else
+        {
+            $res = array(
+                'status' => 'Error',
+                'content' => "Not AJAX request"
+            );
+        }
+
+        return new Response(json_encode($res));
+    }
+
+    public function checkPassAction(Request $request)
+    {
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ($user and $this->getRequest()->isMethod('post'))
+        {
+            $password = $this->getRequest()->get('user_password');
+
+            $encoder_service = $this->get('security.encoder_factory');
+            $encoder = $encoder_service->getEncoder($user);
+            $encoded_pass = $encoder->encodePassword($password, $user->getSalt());
+
+            if ($user->getPassword() == $encoded_pass) {
+
+                $this->get('session')->remove('child');
+                $this->get('session')->remove('time');
+                $this->get('session')->remove('time_setup');
+
+            }
+        }
+        return $this->redirect($this->generateUrl('lebed_video_homepage'));
+    }
 }
