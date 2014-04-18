41c4f340fb286d0ae9f0380873cce9058208d33a
NatashaLebed <natasha.lebed@gmail.com>
2014-04-18 23:23:49 +0300

    refactor VoteVideo

--- src/Lebed/VideoBundle/Controller/DefaultController.php
+++ src/Lebed/VideoBundle/Controller/DefaultController.php
@@ -124,48 +124,6 @@ class DefaultController extends Controller
         return $this->render('LebedVideoBundle:Default:showVideoPage.html.twig', array('video' => $video));
     }
 
-    public function voteVideoAction(Request $request)
-    {
-        $user = $this->getUser();
-
-        if ($this->getRequest()->isXmlHttpRequest())
-        {
-            $video = $this->getDoctrine()->getRepository('LebedVideoBundle:Video')
-                ->find($this->getRequest()->get('video_id'));
-            if($video)
-            {
-                $vote = new Rating();
-                $vote->setUser($user);
-                $vote->setVideo($video);
-                $vote->setWeight($this->getRequest()->get('weight'));
-
-                $this->getDoctrine()->getManager()->persist($vote);
-                $this->getDoctrine()->getManager()->flush();
-
-                $res = array(
-                    'status' => 'Success',
-                    'content' => $video->getVideoRating()
-                );
-            }
-            else
-            {
-                $res = array(
-                    'status' => 'Error',
-                    'content' => "Video not found"
-                );
-            }
-
-        }
-        else
-        {
-            $res = array(
-                'status' => 'Error',
-                'content' => "Not AJAX request"
-            );
-        }
-        return new Response(json_encode($res));
-    }
-
     public function childAction(Request $request)
     {
         if ($this->getRequest()->isXmlHttpRequest())
