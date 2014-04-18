9627488a473af1d45e634a98f5fa4d50ddc31552
NatashaLebed <natasha.lebed@gmail.com>
2014-04-18 12:59:27 +0300

    AddVideo refactoring

--- src/Lebed/VideoBundle/Controller/DefaultController.php
+++ src/Lebed/VideoBundle/Controller/DefaultController.php
@@ -85,40 +85,7 @@ class DefaultController extends Controller
         return $this->render('LebedVideoBundle:Default:index.html.twig', array('videos'=>$videos));
     }
 
-    public  function addVideoAction(Request $request)
-    {
-        $video = new Video();
 
-        $form = $this->createForm(new VideoType(), $video);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $url = $video->getLink();
-            $parsed_url = parse_url($url);
-            parse_str($parsed_url['query'], $parsed_query);
-            $newlink = '<iframe src="http://www.youtube.com/embed/' . $parsed_query['v'] . '" type="text/html" width="400" height="300" frameborder="0"></iframe>';
-            $video->setLink($newlink);
-            $json = json_decode(file_get_contents("http://gdata.youtube.com/feeds/api/videos/".$parsed_query['v'] ."?v=2&alt=jsonc"));
-            $image = new Image();
-            $image->setTitle($video->getTitle());
-            $image->setThumblnail($json->data->thumbnail->sqDefault);
-            $image->setSrc($json->data->thumbnail->hqDefault);
-            $image->setVideo($video);
-           // echo '<img src="' . $json->data->thumbnail->sqDefault . '">';
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($video);
-            $em->persist($image);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('lebed_video_homepage'));
-
-        }
-
-        return $this->render('LebedVideoBundle:Default:addVideo.html.twig',
-            array('video' => $video,
-                  'form' => $form->createView(),
-            ));
-    }
 
     public  function copyVideoToUserAction($video_id)
     {
