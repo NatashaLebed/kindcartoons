f0f765e98775427a667247f3493aaa398b92995c
NatashaLebed <natasha.lebed@gmail.com>
2014-04-18 23:15:33 +0300

    Create addVideo service

--- src/Lebed/VideoBundle/Controller/AddVideoController.php
+++ src/Lebed/VideoBundle/Controller/AddVideoController.php
@@ -5,7 +5,6 @@ namespace Lebed\VideoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Lebed\VideoBundle\Entity\Video;
-use Lebed\VideoBundle\Entity\Image;
 use Lebed\VideoBundle\Form\Type\VideoType;
 
 class AddVideoController extends Controller
@@ -18,27 +17,9 @@ class AddVideoController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-
-            $url = $video->getLink();
-            $parsed_url = parse_url($url);
-            parse_str($parsed_url['query'], $parsed_query);
-            $newlink = '<iframe src="http://www.youtube.com/embed/' . $parsed_query['v'] . '" type="text/html" width="400" height="300" frameborder="0"></iframe>';
-            $video->setLink($newlink);
-            $json = json_decode(file_get_contents("http://gdata.youtube.com/feeds/api/videos/".$parsed_query['v'] ."?v=2&alt=jsonc"));
-
-            $image = new Image();
-            $image->setTitle($video->getTitle());
-            $image->setThumblnail($json->data->thumbnail->sqDefault);
-            $image->setSrc($json->data->thumbnail->hqDefault);
-            $image->setVideo($video);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($video);
-            $em->persist($image);
-            $em->flush();
+           $this->get('add_video.service')->addVideo($video);
 
             return $this->redirect($this->generateUrl('lebed_video_homepage'));
-
         }
 
         return $this->render('LebedVideoBundle:AddVideo:addVideo.html.twig',
